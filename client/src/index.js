f3eee3645bc748da1bfe9118ee363baa332e5dcd
aerojas-ribe <armandorojas@ribe.cl>
2018-11-20 04:28:37 -0300

    Card and documentation snippets

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import css from './assets/sass/index.scss';
 import svgLogo from './assets/img/diversivo-v2.svg';
 import menu from './menu';
+//import questions from "../assets/questions.json"; //The file with questions, represented by a JSON array of Question objects
+
+
 
 console.log(menu.msg);
 
@@ -8,7 +11,17 @@ const logo = new DOMParser().parseFromString(svgLogo, 'application/xml');
 const screen =  document.createElement("DIV");
 const box = document.createElement("DIV");
 const div =  document.createElement("DIV");
-div.appendChild(div.ownerDocument.importNode(logo.documentElement, true))
-box.appendChild(div);
+
+
+//div.appendChild(div.ownerDocument.importNode(logo.documentElement, true))
+
+var cardsFragment = document.createDocumentFragment();
+var i;
+for(i=0;i<5;i++){
+    const card = document.createElement("DIV");
+    card.classList.add('card');
+    cardsFragment.appendChild(card);
+    console.log(i);
+}
+document.body.appendChild(cardsFragment);
 box.classList.add('box');
-document.body.appendChild(box);
