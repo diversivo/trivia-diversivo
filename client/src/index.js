ffaa3ecdb93786514bb73fc57e8e21f62693e790
aerojas-ribe <armandorojas@ribe.cl>
2018-11-27 00:43:34 -0300

    Añadiendo clase Menu
    (untested)
    La clase Menu es una forma de abordar la aplicación en términos de componente. Las otras son Game y Card.
    - Creada clase menu
    - Creado método createPlayButton
    - Creado método createTitle (snippet)
    - Creado método displayMenu (snippet)
    - Snippets en src/index.js de initialize() y main()
    - Reordenada la función isCurrentStateValid() en src/index.js después de aclarar situación con operador ternario y funciones como
    valores de retorno de tal operador.

--- client/src/index.js
+++ client/src/index.js
@@ -23,21 +23,44 @@ let appState = {
 }
 
 //Functions
-const isCurrentStateValid = () => {
-    console.log(appState.currentState);
-    console.log(allowedStates.includes(appState.currentState));
-    allowedStates.includes(appState.currentState) ? () => console.log("Hola") : console.error("Error: the current state of the application is not a valid state: " + appState.currentState + 
-        ". The accepted states are: " + allowedStates.toString() + ".");
-    /* if(allowedStates.includes(appState.currentState)){
+/*Some functions are defined with "traditional" function statements (mostly the larger/more important ones),
+and the reasons are based on this article: https://medium.freecodecamp.org/constant-confusion-why-i-still-use-javascript-function-statements-984ece0b72fd ,
+but the main is to help the editor and the programmer to differentiate and organize quickly the functions and values (constants and variables)
+*/
+
+/**
+ * Evaluates if the current state of the application is valid.
+ *
+ */
+function isCurrentStateValid() {
+    if(allowedStates.includes(appState.currentState)){
         console.log("Hola");
     } else {
         console.error("Error: the current state of the application is not a valid state: " + appState.currentState + 
                       ". The accepted states are: " + allowedStates.toString() + ".");
-    } */
+    }
+}
+
+/**
+ * Initializes the application before the execution
+ *
+ */
+function initializeApp(){
+    appState.currentState = "MENU";
+    //TODO: Add background to the app
+    //TODO: [OPTIONAL] Add music
+}
+
+/**
+ * Application's main function and execution entry point
+ *
+ */
+function main(){
+    initializeApp(); //Initial app state, background, etc.
+    //Show menu and wait for user action
 }
 
 
-//MAIN 
-appState.currentState = "ASDF";
-isCurrentStateValid();
-console.log('H');
+
+//Calling main in order to start execution
+main();
--- client/src/menu/index.js
+++ client/src/menu/index.js
@@ -1,1 +1,38 @@
-export default{msg:'Hola mundo'}
+
+class Menu{
+    constructor(){
+       // this._title = this._createTitle();
+        
+        //playButton: is a DocumentFragment containing the Play! button element of the menu
+        this._playButton = this._createPlayButton();
+    }
+
+    _createTitle(){
+        //TODO make title
+    }
+
+    /**
+     * Creates the main button of the menu - the Play! button
+     */
+    _createPlayButton() {
+        let temporaryFragment = document.createDocumentFragment();
+        let playButton = this.temporaryFragment.createElement("button");
+        playButton.setAttribute("type","button");
+        playButton.setAttribute("id","playButton");
+        temporaryFragment.getElementById("playButton").textContent = "¡Jugar!";
+        return temporaryFragment;
+        //TODO button style
+    }
+
+    /**
+     * Creates the menu and displays it
+     */
+    displayMenu(){
+        let menuFragment = document.createDocumentFragment();
+        //...
+        //TODO make the DIVs for the menu
+        //TODO make basic styles for the menu and menu elements (button, title, etc)
+        menuFragment.appendChild(this.playButton);
+        document.body.appendChild(menuFragment);
+    }
+}
