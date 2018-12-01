0739eee7040df20cd72a5ca6b7cc091783063d46
aerojas-ribe <armandorojas@ribe.cl>
2018-12-01 02:06:01 -0300

    Diseño de menú, parte 1
    - Fondo de la aplicación (incluye algunos cambios en la config de webpack)
    - Reincorporado el logo de Diversivo como parte del diseño del menú
    - Modificado el logo mediante edición en tiempo de ejecución del SVG, agregando sombra y cambiando el color

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,30 @@
-import css from './assets/sass/index.scss';
-import svgLogo from './assets/img/diversivo-v2.svg';
+"use strict";
+import css from "./assets/sass/index.scss";
+import background from "./assets/img/questions1.png";
 import { Menu } from "./menu/index";
 
 //Constants
 const allowedStates = [
-    "MENU",
-    "GAME_LOOP"
+	"MENU",
+	"GAME_LOOP"
 ];
 
 //Application state object
 let appState = {
-    currentState: "MENU",
-    levelsEnabled: false,
-    currentLevel: 1, //Starts at 1
-    maxLevel: 3, //Basically, the number of levels
-    cardsPerLevel: 3, //If levelsEnabled is true, show this number of cards per level
-    cardsIfNoLevel: 10, //If levelsEnabled is false, show this number of cards
-    cardsState: [
-        {
-            id:1,
-            state: 'a, b, c'
-        } //Redefine this card state if neccesary
-    //TODO evaluate if cardsState should pertain to appState or if it could be a property of each card instance
-    ],
-}
+	currentState: "MENU",
+	levelsEnabled: false,
+	currentLevel: 1, //Starts at 1
+	maxLevel: 3, //Basically, the number of levels
+	cardsPerLevel: 3, //If levelsEnabled is true, show this number of cards per level
+	cardsIfNoLevel: 10, //If levelsEnabled is false, show this number of cards
+	cardsState: [
+		{
+			id:1,
+			state: "a, b, c"
+		} //Redefine this card state if neccesary
+		//TODO evaluate if cardsState should pertain to appState or if it could be a property of each card instance
+	],
+};
 
 //Functions
 /*Some functions are defined with "traditional" function statements (mostly the larger/more important ones),
@@ -36,12 +37,12 @@ but the main is to help the editor and the programmer to differentiate and organ
  *
  */
 function isCurrentStateValid() {
-    if(allowedStates.includes(appState.currentState)){
-        console.log("Hola");
-    } else {
-        console.error("Error: the current state of the application is not a valid state: " + appState.currentState + 
+	if(allowedStates.includes(appState.currentState)){
+		console.log("Hola");
+	} else {
+		console.error("Error: the current state of the application is not a valid state: " + appState.currentState + 
                       ". The accepted states are: " + allowedStates.toString() + ".");
-    }
+	}
 }
 
 /**
@@ -49,15 +50,17 @@ function isCurrentStateValid() {
  *
  */
 function initializeApp(){
-    appState.currentState = "MENU";
-    //TODO: Make sure application configurations are valid:
-        // - No negative level numbers
-        // - There should be at least cardsPerLevel questions for each level if levelsEnabled is true
-        // - There should be at least cardsIfNoLevel questions if levelsEnabled is false
-        // - Discount identical questions from the count (related to the two points above) 
-    //TODO: load config from file (validate it previously, if not correct, throw alert and stop execution)
-    //TODO: Add background to the app
-    //TODO: [OPTIONAL] Add music
+	appState.currentState = "MENU";
+	//TODO: Make sure application configurations are valid:
+	// - No negative level numbers
+	// - There should be at least cardsPerLevel questions for each level if levelsEnabled is true
+	// - There should be at least cardsIfNoLevel questions if levelsEnabled is false
+	// - Discount identical questions from the count (related to the two points above) 
+	//TODO: load config from file (validate it previously, if not correct, throw alert and stop execution)
+	//TODO: Add background to the app
+	document.body.style.backgroundImage = "url(" + background + ")";
+	document.body.style.backgroundRepeat = "repeat";
+	//TODO: [OPTIONAL] Add music
 }
 
 /**
@@ -65,9 +68,9 @@ function initializeApp(){
  *
  */
 function main(){
-    initializeApp(); //Initial app state, background, etc.
-    const menu = new Menu();
-    menu.displayMenu();
+	initializeApp(); //Initial app state, background, etc.
+	const menu = new Menu();
+	menu.displayMenu();
 }
 
 
--- client/src/menu/index.js
+++ client/src/menu/index.js
@@ -1,48 +1,89 @@
 import { appState } from "../index"; //TODO undo this circular reference - is used to change state with the Play button
+import svgLogo from "../assets/img/diversivo-v2.svg";
+
 
 class Menu{
-    constructor(){
-        this._title = this._createTitle();
-        //playButton: is a DocumentFragment containing the Play! button element of the menu
-        this._playButton = this._createPlayButton();
-    }
+	constructor(){
+		this._title = this._createTitle();
+		//playButton: is a DocumentFragment containing the Play! button element of the menu
+		this._playButton = this._createPlayButton();
+	}
 
-    _createTitle(){
-        const title = document.createElement("div");
-        title.classList.add("menu_title");
-        title.appendChild(document.createTextNode("Diversivo Trivia"));
-        return title;
-        //TODO title style
-    }
+	_createTitle(){
+		//Container DIV for all elements: a DIV with image inside, and a DIV with text inside
+		const title = document.createElement("div");
+		title.style.alignItems = "center";
+		title.style.marginTop = "12%";
+		title.classList.add("menu_title");
 
-    /**
+		//Image for first DIV - Diversivo's logo with shadow
+		const titleImage = new DOMParser().parseFromString(svgLogo, "application/xml"); //Loading the SVG
+		console.log(titleImage);
+
+		//Inserting the shadow into the SVG
+		titleImage.getElementsByTagName("defs")[0].innerHTML = 
+			"<filter id = \"i1\" width = \"150%\" height = \"150%\"> \
+				<feOffset result = \"offOut\" in = \"SourceAlpha\" dx = \"3\" dy = \"3\"/> \
+				<feBlend in = \"SourceGraphic\" in2 = \"offOut\" mode = \"normal\"/> \
+			</filter>"; //Surely there is a better way to do this (specially for the absence of parsing, maybe with parsing and nodes), but for now, it works
+		titleImage.getElementById("Layer_1").setAttribute("filter", "url(#i1)");
+
+		//First DIV containing Diversivo's logo
+		const titleImageContainer = document.createElement("div");
+		titleImageContainer.classList.add("menu_title", "menu_title_img");
+		titleImageContainer.appendChild(titleImageContainer.ownerDocument.importNode(titleImage.documentElement, true));
+		titleImageContainer.style.padding = "0 25%";
+		//Changing Diversivo's logo color from black to other color
+		titleImageContainer.style.fill = "cadetblue";
+		//TODO solve Diversivo's logo shadow being cut at S and O
+
+		//Guide lines
+		//TODO delete guide lines
+		titleImageContainer.style.border = "5px solid blue";
+
+		//Second DIV containing the "Trivia" word
+		const titleTextContainer = document.createElement("div");
+		titleTextContainer.classList.add("menu_title", "menu_title_text");
+		titleTextContainer.appendChild(document.createTextNode("Trivia"));
+		titleTextContainer.style.textAlign = "center";
+
+		//Apending both DIVs to the containter DIV and returning it
+		title.appendChild(titleImageContainer);
+		title.appendChild(titleTextContainer);
+		return title;
+		//TODO proper title style
+	}
+
+	/**
      * Creates the main button of the menu - the Play! button
      */
-    _createPlayButton() {
-        const playButton = document.createElement("button");
-        playButton.setAttribute("type","button");
-        playButton.setAttribute("id","playButton");
-        playButton.textContent = "¡Jugar!";
-        playButton.addEventListener("click", () => {
-            appState.currentState = "GAME_LOOP";
-            console.log("Current state is GAME_LOOP");
-        });
-        return playButton;
-        //TODO button style
-    }
+	_createPlayButton() {
+		const playButtonContainer = document.createElement("div");
+		playButtonContainer.style.alignContent = "center";
 
-    /**
+		const playButton = document.createElement("button");
+		playButton.setAttribute("type","button");
+		playButton.setAttribute("id","playButton");
+		playButton.textContent = "¡Jugar!";
+		playButton.addEventListener("click", () => {
+			appState.currentState = "GAME_LOOP";
+			console.log("Current state is GAME_LOOP");
+		});
+		return playButton;
+		//TODO button style
+	}
+
+	/**
      * Creates the menu and displays it
      */
-    displayMenu(){
-        const menuFragment = document.createDocumentFragment();
-        //...
-        //TODO make the DIVs for the menu
-        //TODO make basic styles for the menu and menu elements (button, title, etc)
-        menuFragment.appendChild(this._title);
-        menuFragment.appendChild(this._playButton);
-        document.body.appendChild(menuFragment);
-    }
+	displayMenu(){
+		const menuFragment = document.createDocumentFragment();
+		//...
+		//TODO make basic styles for the menu and menu elements (button, title, etc)
+		menuFragment.appendChild(this._title);
+		menuFragment.appendChild(this._playButton);
+		document.body.appendChild(menuFragment);
+	}
 }
 
 export {Menu};
